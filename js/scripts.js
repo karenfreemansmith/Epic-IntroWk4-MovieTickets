fd8c61f8a1a196162ba0448fb797d9afb870fc4a
Ethan Law and Karen Freeman-Smith <ethan@>
2016-08-23 11:14:47 -0700

    add mulit-ticket functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,38 @@
+function Ticket(title, time, age) {
+  this.title = title;
+  this.time = time;
+  this.age = age;
+  this.price=0;
+}
 
-var price = function(time, age) {
-  return time + age;
+Ticket.prototype.getPrice = function(timeVal, ageVal) {
+  this.price = timeVal + ageVal;
+}
+
+var ticketsArray =[];
+
+var totalPriceCalculator = function(tickets) {
+  var total = 0;
+  tickets.forEach(function(ticket){
+    total += ticket.price;
+  });
+  return total;
 }
 
 $("form").submit(function() {
   event.preventDefault();
-  $("#titles").val();
+  var newTicket = new Ticket($("#titles").val(),$("#times :selected").text(), $("#ages :selected").text());
   var time = parseInt($("#times").val());
   var age = parseInt($("#ages").val());
 
-  $("h3.ticket-price").text("$" + price(time, age));
+  newTicket.getPrice(time, age);
+  ticketsArray.push(newTicket);
+  $("#tickets").append("<li>" + newTicket.title+": $" + newTicket.price + "</li>");
+  $("#buyNow").show();
+
+  $("#totalPrice").text(" $" + totalPriceCalculator(ticketsArray));
+});
+
+$("#buyNow").click(function() {
+  $(".receipt").html("Thank you for your Purchase");
 });
